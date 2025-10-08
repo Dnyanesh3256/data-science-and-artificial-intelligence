d4a5f2c0bf1a7b1f57a2c067a5d354b801dd7bbe
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-10-08 21:41:17 +0530

    strings and type conversion in python

---
+++ python/strings-and-type-conversion/ord-and-chr.py
@@ -0,0 +1,7 @@
+# ord() and chr() functions
+
+a = 65
+print(chr(a))
+
+b = "b"
+print(ord(b))
---
+++ python/strings-and-type-conversion/type-conversion.py
@@ -0,0 +1,14 @@
+a = 25
+print(type(a))
+
+a = str(a)
+print(type(a))
+
+isOpen = 0
+isOpen = bool(isOpen)
+print(isOpen, type(isOpen))
+
+print("falsy values in python : false, 0, 0.0, "", [], (), {}")
+
+print("implicit type casting : ")
+print(12/4)
