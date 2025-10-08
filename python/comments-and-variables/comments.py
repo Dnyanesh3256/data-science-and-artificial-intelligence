071c8084d5f2f5dfd51598dbcacdcf03de8e218e
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-10-08 17:27:05 +0530

    comments and variables in python

---
+++ python/comments-and-variables/comments.py
@@ -0,0 +1,10 @@
+print("comments are something that are ignored by python interpreter!!")
+
+print("we have to use # for writing a comment in python.")
+
+print("multiline comments are not available in python but we can achieve it by using Doc String")
+
+# comment
+
+"""multiline comment
+DOC STRING"""
---
+++ python/comments-and-variables/variables.py
@@ -0,0 +1,6 @@
+print("in python variables are used as a storage to store things in python")
+
+print("Naming Conventions : ")
+print("     1. Camel Case : dbTupe")
+print("     2. Pascal Case : DBTupe")
+print("     3. Snake Case : d_b_tupe")
