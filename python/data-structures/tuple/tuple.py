5e3e9687b68736a3f059139b45f2cb1e85d89cde
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-10-11 21:35:08 +0530

    tuple in python

---
+++ python/data-structures/tuple/tuple.py
@@ -0,0 +1,18 @@
+t = (1, 2, 3, 4, 4) # duplicates allowed
+
+for i in range(len(t)):
+    print(t[i])
+
+t1 = (11, 22, 33, print(), True) # possible
+
+print(t.index(3))
+print(t.count(1))
+
+print(t[3])
+# t[4] = 5 //not possible
+print(t)
+# t[1] = 3 //not possible
+
+a, b, c, d = (2, 4, 6, 8)
+
+print(c)
